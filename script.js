9463c5d44c620df077661d72694898bade224f2f
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-07-16 15:27:29 +0530

    feat: add ability to do basic operations

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
+const prevExpression = document.querySelector('#prev-expression');
 const currentExpression = document.querySelector('#current-expression');
+const equalButton = document.querySelector('#equal-button');
+
+equalButton.addEventListener('click', handleEqualButton);
+
 const digitButtons = Array.from(document.querySelectorAll('.button-digit'));
-const operatorButtons = Array.from(document.querySelectorAll('.button-operator'));
-
-
 digitButtons.forEach(item => item.addEventListener('click', addDigit));
+
+const operatorButtons = Array.from(document.querySelectorAll('.button-operator'));
 operatorButtons.forEach(item => item.addEventListener('click', addOperator));
 
 
 
 let operator = ''
   , operand1 = ''
-  , operand2 = '';
+  , operand2 = ''
+  , result;
+
+
+function add(operand1, operand2) {
+  return operand1 + operand2;
+}
+
+function subtract(operand1, operand2) {
+  return operand1 - operand2;
+}
+
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
+}
+
+function divide(operand1, operand2) {
+  return operand1 / operand2;
+}
+  
+  
+
+function operate(operator, operand1, operand2) {
+  switch (operator) {
+    case '+':
+      return add(operand1, operand2);
+  
+    case '-':
+      return subtract(operand1, operand2);
+  
+    case '*':
+      return multiply(operand1, operand2);
+  
+    case '/':
+      return divide(operand1, operand2);
+  
+    default:
+      break;
+  }
+}
+
+
 
 function updateDisplay() {
   currentExpression.textContent = operand1;
@@ -29,5 +74,14 @@ function addDigit() {
 function addOperator() {
   operator = this.textContent;
   currentExpression.textContent += operator;
+
+}
+
+function handleEqualButton() {
+  result = operate(operator, operand1, operand2);
+  currentExpression.textContent = result;
+  prevExpression.textContent = `${operand1} ${operator} ${operand2} = ${result}`;
+  operand1 = result;
+  operand2 = '';
 
 }
